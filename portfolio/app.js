31dc2877db6f313d9050c4fa87afded84c6ec98a
Parth Shrivastava <shrivastavaparth03@gmail.com>
2025-07-16 12:00:44 +0530

    Add Sidebar and Responsiveness to portfolio

--- portfolio/app.js
+++ portfolio/app.js
@@ -2,9 +2,14 @@ const video1 = document.getElementById("projectVideo1");
 const video2 = document.getElementById("projectVideo2");
 const video3 = document.getElementById("projectVideo3");
 
-const videoList = [video1, video2, video3];
+// Sidebar elements //
+const sideBar = document.querySelector(".sidebar");
+const menu = document.querySelector(".menu-icon");
+const closeIcon = document.querySelector(".close-icon");
 
 const hoverSign = document.querySelector(".hover-sign");
+
+const videoList = [video1, video2, video3];
 
 videoList.forEach(function (video) {
   video.addEventListener("mouseover", function () {
@@ -15,4 +20,15 @@ videoList.forEach(function (video) {
     video.pause();
     hoverSign.classList.remove("active");
   });
+});
+
+// Sidebar elements //
+menu.addEventListener("click", function () {
+  sideBar.classList.remove("close-sidebar");
+  sideBar.classList.add("open-sidebar");
+});
+
+closeIcon.addEventListener("click", function () {
+  sideBar.classList.remove("open-sidebar");
+  sideBar.classList.add("close-sidebar");
 });
